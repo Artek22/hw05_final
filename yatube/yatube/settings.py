14ace6651bdea6cf13fd12e0e5faa9188c8af372
Андрей <temp60@mail.ru>
2023-03-06 17:35:32 +0300

    Update settings.py

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECRET_KEY = os.environ.get("SECRET_KEY")
-SECRET_KEY = os.environ.get("SECRET_KEY", 'asdfsadf')
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = True
 
